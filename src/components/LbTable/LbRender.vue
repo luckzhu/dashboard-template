4cfc88073996ba9724a7c98903f0adb01b3eacc7
luckzhu <luckzhu1992@gmail.com>
2020-11-30 16:00:09 +0800

    新增LbTable组件

---
+++ src/components/LbTable/LbRender.vue
@@ -0,0 +1,16 @@
+/* eslint-disable */
+<script>
+export default {
+  name: 'LbRender',
+  functional: true,
+  props: {
+    // eslint-disable-next-line
+    scope: Object,
+    // eslint-disable-next-line
+    render: Function
+  },
+  render: (h, ctx) => {
+    return ctx.props.render ? ctx.props.render(h, ctx.props.scope) : ''
+  }
+}
+</script>
